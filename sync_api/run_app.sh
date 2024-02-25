00e671f7e436707629ba0d00d91ca1398bb5ea77
manishasodekar <manish@healiom.com>
2024-02-25 14:29:21 +0530

    Update run_app.sh

--- sync_api/run_app.sh
+++ sync_api/run_app.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Set environment and corresponding port
-if [ "$1" == "stage" ]; then
-    ENVIRONMENT="stage"
-    PORT=8502
-elif [ "$1" == "dev" ]; then
+if [ "$1" == "dev" ]; then
     ENVIRONMENT="dev"
     PORT=8501
+elif [ "$1" == "stage" ]; then
+    ENVIRONMENT="stage"
+    PORT=8502
+elif [ "$1" == "prod" ]; then
+    ENVIRONMENT="prod"
+    PORT=8503
 else
-    echo "Invalid environment. Please specify 'stage' or 'dev'."
+    echo "Invalid environment. Please specify 'stage' or 'dev' or 'prod'."
     exit 1
 fi
 
